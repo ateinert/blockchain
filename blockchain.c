2a0cc6ca89b579d8a0ade4149c102c0c511e049b
ateinert <44173880+ateinert@users.noreply.github.com>
2018-12-02 17:50:10 -0600

    Update blockchain.c

--- blockchain.c
+++ blockchain.c
@@ -1,5 +1,4 @@
 //blockchain.cpp
-#include <vector>
 #include <sys/types.h>
 #include <sys/signal.h>
 #include <sys/socket.h>
@@ -57,31 +56,6 @@ void updateTransactionCount(int count)
 	file.open("chain/transactionCount");
 	file << count;
 	transactionCount = count;
-}
-
-void loadWallet()
-{
-	FILE *file;
-	file = fopen("chain/wallet", "r");
-	wallet.clear();
-	Transaction temp;
-	while (fread(&temp, sizeof(temp), 1, file))
-	{
-		wallet.push_back(temp);
-	}
-	fclose(file);
-}
-
-void updateWallet(vector<Transaction> list)
-{
-	FILE *file;
-	file = fopen("chain/wallet", "w");
-	for (int i = 0; i < list.size(); i++)
-	{
-		fwrite (&list[i], sizeof(Transaction), 1, file); 
-	}
-	fclose(file);
-	wallet = list;
 }
 
 Block recieveBlock(int fd)
@@ -290,135 +264,6 @@ Transaction loadTransactionFromFile(string str)
 	// close file 
 	fclose(file); 
 	return trans;
-}
-
-void userInterface(ostream& os, int blockCount, int currentTransactionCount)
-{
-	os << "\033[1J";
-	os << "\033[1;1H";
-	os << "Current Block Count: " << setw(7) << " " << blockCount << endl;
-	os << "Current Transaction Count: " << currentTransactionCount << endl;
-	//os << "Most Recent Event: " << recentEvent << endl;
-}
-
-vector<Transaction> publishTransactions(string id, char *publisherID, char *gameID, int number, string socket, vector<string> hostList)
-{
-	char license[129];
-	char key[65];
-	int len = 64;
-	static const char alphanum[] =
-        "0123456789"
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        "abcdefghijklmnopqrstuvwxyz";
-	vector<Transaction> t;
-	loadTransactionCount;
-	for (int i = 0; i < number; i++)
-	{
-		for (int j = 0; j < len; ++j) {
-			key[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
-		}
-		key[64] = '\0';
-		strcat(license, publisherID);
-		strcat(license, gameID);
-		strcat(license, key);
-		
-		license[128] = '\0';
-		Transaction temp;
-		//publish license using transaction
-		broadcastTransaction(temp, socket, hostList);
-		t.push_back(temp);
-		updateTransactionCount(transactionCount+1);
-	}
-	return t;
-}
-
-void client(vector<string> hosts, vector<Transaction> wallet, string service)
-{
-	// this will only send and create new transactions automatically no user input;
-	// occurs every 3 seconds
-	loadTransactionCount();
-	updateTransactionCount(transactionCount+1);
-	Transaction temp;
-	// then point it to the previous transactions
-	broadcastTransaction(temp, service, hosts);
-}
-
-void server()
-{
-	// child server	
-	// create different ports for recieving transactions, blocks and the chain settings requests
-	int msock1, msock2;
-	int ssock;
-	int alen;
-	sockaddr_in fsin;
-	
-	// listen for transaction
-	msock1 = passiveTCP(TRANSACTION_PORT, QLEN);
-	// listen for block
-	msock2 = passiveTCP(BLOCK_PORT, QLEN);
-	// listen for chain
-	(void) signal(SIGCHLD, reaper);
-	while (1)
-	{
-		alen = sizeof(fsin);
-		// fork for each one
-		if (fork() == 0)
-		{
-			//child 1
-			ssock = accept(msock1, (sockaddr *)&fsin, (socklen_t *)&alen);
-			if (ssock < 0) 
-			{
-				if (errno == EINTR)   //system call was interrupted permaturely with a signal before it was able to complete
-					continue;
-				cerr << "socket failure" << endl;
-				exit(EXIT_FAILURE);
-			}
-			switch (fork()) 
-			{
-				case 0:		/* child */
-					(void) close(msock1);
-					recieveTransaction(ssock);
-					exit(0);
-				default:	/* parent */
-					(void) close(ssock);
-					break;
-				case -1:
-					exit(1);
-			}
-		}
-		else
-		{
-			if (fork() == 0)
-			{
-				//child 2
-				ssock = accept(msock2, (sockaddr *)&fsin, (socklen_t *)&alen);
-				if (ssock < 0) 
-				{
-					if (errno == EINTR)   //system call was interrupted permaturely with a signal before it was able to complete
-						continue;
-					cerr << "socket failure" << endl;
-					exit(EXIT_FAILURE);
-				}
-				switch (fork()) 
-				{
-					case 0:		/* child */
-						(void) close(msock2);
-						recieveBlock(ssock);
-						exit(0);
-					default:	/* parent */
-						(void) close(ssock);
-						break;
-					case -1:
-						exit(1);
-				}
-			}
-			else
-			{
-				break;
-			}
-			break;
-		}
-	}
 }
 
 void reaper(int sig)
