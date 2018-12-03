e1e3ec9464c338dbdb534c12998259a27c68e55a
ateinert <44173880+ateinert@users.noreply.github.com>
2018-12-02 18:37:11 -0600

    Update main.c

--- main.c
+++ main.c
@@ -10,6 +10,9 @@
 #include <string.h>
 #include "blockchain.h"
 
+char id[] = "7kzf5fl8dX7lsUfUDSvCcBeAoUVZyfWefr02SzYVkXWjsgDWCRJgHeCAoCkUqRuLK0N5fQLG7fqimZwBLAL40hYCaFwNbjmB1KlNPtm8pwQDSehXzgAPKFPaqPR8KFJDTLcxzSwseZ2fsBHbzZgpud909z2yA5u8kkM9HEAYB7Au7YzMAl9WWAdRkK0ESZwF70RGBdNkOASOx2okb29SVnNDOqG7t7B5LqveNfRtJRqkcHYNlUnsymES3DGnfi6p";
+char license[] = "VrGNpz87DHaYlp97cWS7JFpxlRNruIw9xPGWRfrzHznzwDzw0YB8d4SiEzzI3ewwbGSVUk6aUu66E4olapwvmHuBeg6kBItcuEljq6kFPEuykvaQ0BjmaXCVDwOAkdaK";
+
 int main(int argc, char **argv)
 {
 	//chdir("cd ~/.blockchain");
@@ -49,10 +52,18 @@ int main(int argc, char **argv)
 			printf("Press ENTER to send a block and 10 transactions");
 			getchar();
 			char buffer[65];
-			sha256_file(itoa(blockcount), buffer);
+			strcpy(buffer, "0");
+			if (blockcount > 0)
+				sha256_file(itoa(blockcount), buffer);
 			Block block = createBlock(blockcount, buffer);
 			blockcount++;
 			broadcastBlock(block, hosts, service, numHosts);
+			for (i = 0; i < 10; i++)
+			{
+				strcpy(buffer, "0");
+				Transaction trans = createTransaction(transactioncount, blockcount, buffer);
+				transactioncount++;
+			}
 		}
 	}
 	else
