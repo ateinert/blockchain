00ea445a286c631486076fd7b3199bba23d93191
ateinert <44173880+ateinert@users.noreply.github.com>
2018-12-02 19:48:22 -0600

    Update blockchain.h

--- blockchain.h
+++ blockchain.h
@@ -11,22 +11,28 @@ typedef struct {
 typedef struct {
 	bool temp;
 	int transactionCount;
+	char transactionTitle[256];
 	char prevHash[65];
 	char ownerKey[257];
 	char license[129];
 	time_t creationTime;
 } Transaction;
 
+void server(char*);
+
 int blockValidate(Block);
 int transactionValidate(Transaction);
 
+Block createBlock(int, char[65]);
+Block createTransaction(int, int, char[65], char[257], char[129);
+
 Block recieveBlock(int);
-void broadcastBlock(Block, char**, char*);
+void broadcastBlock(Block, char**, char*, int);
 void saveBlockToFile(Block);
 Block loadBlockFromFile(char*);
 
 Transaction recieveTransaction(int);
-void broadcastTransaction(Transaction, char**, char*);
+void broadcastTransaction(Transaction, char**, char*, int);
 void saveTransactionToFile(Transaction);
 Transaction loadTransactionFromFile(char*);
 
